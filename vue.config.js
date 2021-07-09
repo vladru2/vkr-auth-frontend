579a3e0f56d89082c3088fe53adde2b507841f98
VinTarZ <vintarz@yandex.ru>
2021-07-09 16:46:57 +0300

    Local API proxy

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,22 @@
 module.exports = {
-  pluginOptions: {
-    i18n: {
-      locale: 'ru',
-      fallbackLocale: 'ru',
-      localeDir: 'locales',
-      enableInSFC: true,
+    pluginOptions: {
+        i18n: {
+            locale: 'ru',
+            fallbackLocale: 'ru',
+            localeDir: 'locales',
+            enableInSFC: true,
+        },
     },
-  },
+    devServer: {
+        proxy: {
+            '^/api': {
+                target: 'http://127.0.0.1:28981/auth',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': '/',
+                },
+                headers: { 'Hcs-Client-IP': '127.0.0.1' },
+            },
+        },
+    },
 }
